df4e54f763efa132d528e260f8236e4da9247686
Frokesy <akindeleayanfeoluwa@gmail.com>
2024-09-12 11:28:53 +0100

    hero section WIP

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,45 +3,47 @@ import { Globe, Location } from "./icons";
 
 const Hero = () => {
   return (
-    <div className="bg-gradient-to-r from-[#292727] text-[#fff] to-[#424040] min-h-[100vh]">
+    <div className="bg-gradient-to-r from-[#292727] relative text-[#fff] to-[#424040] min-h-[100vh]">
       <Header />
-      <div className="relative">
-        <h2 className="text-[220px] uppercase tracking-widest font-semibold -mt-10 w-[90vw] mx-auto">
-          Freelance
-        </h2>
 
-        <div className="flex justify-between pl-20">
-          <div className="w-[60%] z-10 space-y-20">
-            <h2 className="text-[70px] w-[90%] capitalize font-semibold">
-              Elevate your projects with elite freelancers
-            </h2>
-            <p className="text-[30px] font-semibold">
-              Discover skilled talent. Take your job to the next level. Partner
-              with our curated network of professionals, equipped to elevate
-              your tech and design initiatives to new heights.
-            </p>
+      <div className="w-[85vw] mx-auto">
+        <div className="flex items-center justify-center space-x-6 mt-6 text-[100px] font-semibold">
+          <h2>The Future of</h2>
+          <img src="/assets/hero_one.png" alt="hero" className="w-[230px]" />
+          <h2>Freelance</h2>
+        </div>
+        <p className="text-[32px] text-center font-semibold">
+          Discover skilled talent. Take your job to the next level. Partner with
+          our curated network of professionals, equipped to elevate your tech
+          and design initiatives to new heights.
+        </p>
 
-            <div className="w-[75%]">
-              <div className="bg-[#444242] shadow-2xl rounded-lg px-4 text-[20px] flex justify-between">
-                <div className="flex space-x-2 items-center border-r-[3px] border-[#808080] py-3 w-[25%]">
-                  <Location />
-                  <p className="">Location</p>
-                </div>
-                <div className="flex space-x-2 items-center w-[70%]">
-                  <Globe />
-                  <p>What services are you looking for?</p>
-                </div>
-              </div>
-
-              <button className="text-center bg-[#fff] rounded-lg py-3 w-[100%] text-[#1C5D99] mt-6 mb-[10vh]">Find Talent</button>
-            </div>
+        <div className="border-4 border-[#808080] flex items-center justify-between mt-10 rounded-lg mx-16">
+          <div className="flex space-x-3 items-center w-[15%] border-r-2 bordeer-[#808080] pl-3">
+            <Location />
+            <p>Location</p>
           </div>
-          <div className="w-[1000px] h-[950px]">
-            <img
-              src="/assets/hero.png"
-              alt="hero"
-              className="absolute top-[15vh] right-6 h-[100%]"
+          <div className="flex items-center space-x-3 w-[60%]">
+            <Globe />
+            <input
+              type="text"
+              placeholder="What services are you looking for?"
+              className="bg-inherit border-none outline-none placeholder:text-[#fff]"
             />
+          </div>
+          <div className="text-[#1c5d99] w-[20%] bg-[#fff] py-4 flex items-center rounded-lg font-semibold justify-center">
+            <h2>Find Talent</h2>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-[100%] mt-[30vh] relative">
+        <img src="/assets/cloud.png" className="w-[100%]" alt="img" />
+        <div className="flex items-center justify-center ">
+          <div className="absolute bottom-0 flex items-start">
+            <img src="/assets/arrow_left.png" alt="img" />
+            <img src="/assets/hero_two.png" alt="img" className="" />
+            <img src="/assets/arrow_right.png" alt="img" />
           </div>
         </div>
       </div>
